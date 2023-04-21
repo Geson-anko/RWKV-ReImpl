776e434054aca62b144050c8d3632980a2eeaaf8
myxyy <m.myxomatosis@gmail.com>
2023-04-22 00:48:01 +0900

    コンストラクタがコンテキスト長に依存しないよう修正

--- tests/models/components/test_channel_mixing.py
+++ tests/models/components/test_channel_mixing.py
@@ -8,13 +8,13 @@ from src.models.components.channel_mixing import ChannelMixing
     dim,
     """,
     [
+        (1024,512),
         (1024,1024),
         (2048,1024),
-        (1024,512),
     ]
 )
 def test_channel_mixing(len, dim):
-    channel_mixing = ChannelMixing(len, dim)
+    channel_mixing = ChannelMixing(dim)
     x = torch.rand(len, dim)
     y = channel_mixing(x)
     assert y.size() == torch.Size([len, dim])
