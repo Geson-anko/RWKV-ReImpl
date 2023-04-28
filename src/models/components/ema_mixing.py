b065e2690969777d072904c1c2dc1adcc52dfee6
myxyy <m.myxomatosis@gmail.com>
2023-04-28 14:10:12 +0000

    EMAMixingバッチ対応

--- src/models/components/ema_mixing.py
+++ src/models/components/ema_mixing.py
@@ -12,24 +12,28 @@ class EMAMixing(nn.Module):
         self.sigmoid = nn.Sigmoid()
         self.x_mix_last = None
 
-    # (len, dim) -> (len, dim)
+    # (len, batch, dim) -> (len, batch, dim)
     def forward(self, x: Tensor) -> Tensor:
         len = x.shape[0]
         if self.x_mix_last is None:
-            self.clear_hidden()
-        factor = self.sigmoid(self.factor)
-        factor_progression = torch.pow(factor, torch.arange(len).unsqueeze(-1))
-        fft_factor_progression = torch.fft.rfft(factor_progression, n=len * 2, dim=0)
-        fft_x = torch.fft.rfft(x, n=len * 2, dim=0)
-        x_conv_factor_progression = torch.fft.irfft(fft_x * fft_factor_progression, dim=0).narrow(
+            self.x_mix_last = torch.zeros(
+                x.shape[1:], dtype=x.dtype, device=x.device
+            )  # (batch, dim)
+        factor = self.sigmoid(self.factor)  # (dim)
+        factor_progression = torch.pow(factor, torch.arange(len).unsqueeze(-1))  # (len, dim)
+        fft_factor_progression = torch.fft.rfft(factor_progression, n=len * 2, dim=0)  # (?, dim)
+        fft_x = torch.fft.rfft(x, n=len * 2, dim=0)  # (?, batch, dim)
+        x_conv_factor_progression = torch.fft.irfft(
+            fft_x * fft_factor_progression.unsqueeze(1), dim=0
+        ).narrow(
             0, 0, len
-        )
+        )  # (len, batch, dim)
         x_mix = (
-            self.x_mix_last * factor_progression * factor
+            self.x_mix_last.unsqueeze(0) * factor_progression.unsqueeze(1) * factor
             + (1 - factor) * x_conv_factor_progression
         )
         self.x_mix_last = x_mix[-1]
         return self.W(x_mix)
 
     def clear_hidden(self):
-        self.x_mix_last = torch.zeros(self.dim)
+        self.x_mix_last = None
--- tests/models/components/test_ema_mixing.py
+++ tests/models/components/test_ema_mixing.py
@@ -7,20 +7,21 @@ from src.models.components.ema_mixing import EMAMixing
 @pytest.mark.parametrize(
     """
     len,
+    batch,
     dim,
     """,
     [
-        (1024, 512),
-        (1024, 1024),
-        (2048, 1024),
+        (1024, 1, 512),
+        (1024, 4, 1024),
+        (2048, 16, 1024),
     ],
 )
-def test_ema_mixing(len, dim):
+def test_ema_mixing(len, batch, dim):
     ema_mixing = EMAMixing(dim)
-    x = torch.rand(len, dim)
+    x = torch.rand(len, batch, dim)
     x = ema_mixing(x)
     x = ema_mixing(x)
     ema_mixing.clear_hidden()
     x = ema_mixing(x)
     x = ema_mixing(x)
-    assert x.size() == torch.Size([len, dim])
+    assert x.size() == torch.Size([len, batch, dim])
