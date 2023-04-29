d1d5ad502ff6dcb2b7df93a397bde389d1727484
myxyy <m.myxomatosis@gmail.com>
2023-04-29 06:17:38 +0000

    TimeMixing実装

---
+++ tests/models/components/test_time_mixing.py
@@ -0,0 +1,27 @@
+import pytest
+import torch
+
+from src.models.components.time_mixing import TimeMixing
+
+
+@pytest.mark.parametrize(
+    """
+    len,
+    batch,
+    dim,
+    """,
+    [
+        (1024, 1, 512),
+        (1024, 4, 1024),
+        (2048, 16, 1024),
+    ],
+)
+def test_time_mixing(len, batch, dim):
+    time_mixing = TimeMixing(dim)
+    x = torch.rand(len, batch, dim)
+    x = time_mixing(x)
+    x = time_mixing(x)
+    time_mixing.clear_hidden()
+    x = time_mixing(x)
+    x = time_mixing(x)
+    assert x.size() == torch.Size([len, batch, dim])
