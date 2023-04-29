75c1e90befdfce4ff1c7197a3b357c7456cfb7f4
myxyy <m.myxomatosis@gmail.com>
2023-04-29 05:06:40 +0000

    PreviousMixingに重み入れるの忘れてた

--- src/models/components/previous_mixing.py
+++ src/models/components/previous_mixing.py
@@ -8,16 +8,17 @@ class PreviousMixing(nn.Module):
         super().__init__()
         self.dim = dim
         self.mix_factor = nn.Parameter(torch.rand(dim))
+        self.W = nn.Linear(dim, dim, bias=False)
         self.x_last = None
 
-    # (len, *, dim) -> (len, *, dim)
+    # (len, dim) -> (len, dim)
     def forward(self, x: Tensor) -> Tensor:
         if self.x_last is None:
-            self.x_last = torch.zeros(x.shape[1:], dtype=x.dtype, device=x.device)
+            self.clear_hidden()
         x_shift = x.roll(shifts=1, dims=0)
         x_shift[0] = self.x_last
         self.x_last = x[-1]
-        return x * self.mix_factor + x_shift * (1 - self.mix_factor)
+        return self.W(x * self.mix_factor + x_shift * (1 - self.mix_factor))
 
     def clear_hidden(self):
-        self.x_last = None
+        self.x_last = torch.zeros(self.dim)
