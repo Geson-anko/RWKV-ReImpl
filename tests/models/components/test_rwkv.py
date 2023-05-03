0e42c2e1c4c93c1cec6bc071afa13d6b2fd99fad
myxyy <m.myxomatosis@gmail.com>
2023-05-03 03:18:56 +0000

    RWKV本体実装

---
+++ tests/models/components/test_rwkv.py
@@ -0,0 +1,28 @@
+import pytest
+import torch
+
+from src.models.components.rwkv import RWKV
+
+
+@pytest.mark.parametrize(
+    """
+    len,
+    batch,
+    dim,
+    depth,
+    """,
+    [
+        (1024, 1, 512, 2),
+        (1024, 4, 1024, 1),
+        (512, 16, 256, 3),
+    ],
+)
+def test_rwkv(len, batch, dim, depth):
+    rwkv = RWKV(dim, depth)
+    x = torch.rand(len, batch, dim)
+    x = rwkv(x)
+    x = rwkv(x)
+    rwkv.clear_hidden()
+    x = rwkv(x)
+    x = rwkv(x)
+    assert x.size() == torch.Size([len, batch, dim])
