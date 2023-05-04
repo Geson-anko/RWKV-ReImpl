0eef6779d4b242789480750eee34e1f3d1c5bcc4
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-05-04 10:51:55 +0000

    Format by pre-commit

--- tests/utils/test_generations.py
+++ tests/utils/test_generations.py
@@ -1,7 +1,9 @@
-from src.utils.generations import rwkv_generate_fixed_length
-from src.models.components.rwkv_lang import RWKVLang, RWKV
 import pytest
 import torch
+
+from src.models.components.rwkv_lang import RWKV, RWKVLang
+from src.utils.generations import rwkv_generate_fixed_length
+
 
 @pytest.mark.parametrize(
     """
@@ -12,7 +14,7 @@ import torch
     [
         (32, 64, True),
         (32, 128, False),
-    ]
+    ],
 )
 def test_rwkv_generate_fixed_length(
     dim: int,
@@ -26,17 +28,10 @@ def test_rwkv_generate_fixed_length(
         vocab_size=vocab_size,
     )
 
-    prompt_tokens = torch.randint(1, vocab_size, (num_generating_tokens // 2,),dtype=torch.long)
+    prompt_tokens = torch.randint(1, vocab_size, (num_generating_tokens // 2,), dtype=torch.long)
 
     generated_tokens = rwkv_generate_fixed_length(
-        net,
-        prompt_tokens,
-        num_generating_tokens,
-        prepare_hidden_state_recursively
+        net, prompt_tokens, num_generating_tokens, prepare_hidden_state_recursively
     )
     assert generated_tokens.shape == (num_generating_tokens,)
     assert generated_tokens.dtype == torch.long
-
-
-
-
