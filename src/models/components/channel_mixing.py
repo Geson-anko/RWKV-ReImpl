92bbc2f42aae16133b94cf1b31669c1f509119be
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-05-18 08:56:04 +0000

    ADD hidden dim factor

--- src/models/components/channel_mixing.py
+++ src/models/components/channel_mixing.py
@@ -6,13 +6,15 @@ from .previous_mixing import PreviousMixing
 
 
 class ChannelMixing(nn.Module):
-    def __init__(self, dim: int):
+    def __init__(self, dim: int, hidden_dim_factor: int = 4):
         super().__init__()
         self.dim = dim
+        self.hidden_dim_factor = hidden_dim_factor
+        hidden_dim = dim * hidden_dim_factor
         self.previous_mixing_k = PreviousMixing(dim)
         self.previous_mixing_r = PreviousMixing(dim)
-        self.Wv = nn.Linear(dim, dim, bias=False)
-        self.Wk = nn.Linear(dim, dim, bias=False)
+        self.Wk = nn.Linear(dim, hidden_dim, bias=False)
+        self.Wv = nn.Linear(hidden_dim, dim, bias=False)
         self.Wr = nn.Linear(dim, dim, bias=False)
         self.sigmoid = nn.Sigmoid()
         self.relu = nn.ReLU()
